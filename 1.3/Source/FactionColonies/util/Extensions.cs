ac7e3c57512bccd78ce97963a77290534b5e2666
Daniel Plaster <daniel_plaster@live.de>
2021-10-10 11:00:28 +0200

    added comments

--- 1.3/Source/FactionColonies/util/Extensions.cs
+++ 1.3/Source/FactionColonies/util/Extensions.cs
@@ -4,9 +4,15 @@ namespace FactionColonies.util
 {
 	static class Extensions
 	{
+		/// <summary>
+		///		Checks if a given <c>PawnKindDef</c> <paramref name="pawnKindDef"/> is and Animal and if it is not blacklisted by tradeTag 
+		/// </summary>
+		/// <param name="pawnKindDef"></param>
+		/// <returns></returns>
 		public static bool IsAnimalAndAllowed(this PawnKindDef pawnKindDef)
 		{
-			return pawnKindDef.race.race.Animal && pawnKindDef.RaceProps.IsFlesh && pawnKindDef.race.tradeTags != null &&
+			return pawnKindDef.race.race.Animal && pawnKindDef.RaceProps.IsFlesh && pawnKindDef.race.tradeTags != null && 
+									pawnKindDef.race.race.animalType != AnimalType.Dryad &&
 									!pawnKindDef.race.tradeTags.Contains("AnimalDryad") &&
 									!pawnKindDef.race.tradeTags.Contains("AnimalMonster") &&
 									!pawnKindDef.race.tradeTags.Contains("AnimalGenetic") &&
