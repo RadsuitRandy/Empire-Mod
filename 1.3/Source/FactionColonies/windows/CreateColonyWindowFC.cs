5d7183deed02366342cb42fae1f6acc1d1aa39d2
Daniel Plaster <daniel_plaster@live.de>
2021-10-13 18:26:23 +0200

    fixed being able to place settlements side by side

--- 1.3/Source/FactionColonies/windows/CreateColonyWindowFC.cs
+++ 1.3/Source/FactionColonies/windows/CreateColonyWindowFC.cs
@@ -171,7 +171,7 @@ namespace FactionColonies
                 if (PaymentUtil.getSilver() >= silverToCreateSettlement) //if have enough monies to make new settlement
                 {
                     StringBuilder reason = new StringBuilder();
-                    if (currentTileSelected == -1 || !TileFinder.IsValidTileForNewSettlement(currentTileSelected, reason) || 
+                    if (currentTileSelected == -1 || AnyWorldSettlementFCAtOrAdjacent(currentTileSelected, reason) || !TileFinder.IsValidTileForNewSettlement(currentTileSelected, reason) ||
                         Find.World.GetComponent<FactionFC>().checkSettlementCaravansList(currentTileSelected.ToString()))
                     {
                         //Alert Error to User
@@ -234,5 +234,20 @@ namespace FactionColonies
             Widgets.Label(new Rect(x, y + height / 2, length, height / 2f), text2);
         }
 
+
+        private bool AnyWorldSettlementFCAtOrAdjacent(int tile, StringBuilder reason = null)
+        {
+            foreach (WorldSettlementFC settlement in Find.WorldObjects.AllWorldObjects.Where(obj => obj.GetType() == typeof(WorldSettlementFC)))
+            {
+                Log.Message("Type: " + settlement.GetType().Name);
+                if (Find.WorldGrid.IsNeighborOrSame(settlement.Tile, tile))
+                {
+                    reason?.Append("FactionBaseAdjacent".Translate());
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
