622b0def1d64837603946c920f67760fb0955190
Daniel Plaster <daniel_plaster@live.de>
2021-11-07 14:12:56 +0100

    added a new class TicksExtensions

---
+++ 1.3/Source/FactionColonies/util/Extensions/TicksExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+
+namespace FactionColonies.util
+{
+    static class TicksExtensions
+    {
+        public static string ToTimeString(this int ticks)
+        {
+            string minutes = "" + ticks / 3600;
+            string seconds = "" + ticks % 3600 / 60;
+
+            while (minutes.Length < 2) minutes = "0" + minutes;
+            while (seconds.Length < 2) seconds = "0" + seconds;
+
+            if (ticks < 60000) return minutes + ":" + seconds + " mins/secs";
+            return GenDate.ToStringTicksToDays(ticks);
+        }
+
+    }
+}
