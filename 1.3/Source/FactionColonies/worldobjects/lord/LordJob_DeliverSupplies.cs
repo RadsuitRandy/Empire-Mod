b7276ccfc196e2983539e9980a8ce67dd213fca5
Daniel Plaster <daniel_plaster@live.de>
2021-11-19 18:04:31 +0100

    small optimizations in LordJob_DeliverSupplies

--- 1.3/Source/FactionColonies/worldobjects/lord/LordJob_DeliverSupplies.cs
+++ 1.3/Source/FactionColonies/worldobjects/lord/LordJob_DeliverSupplies.cs
@@ -29,81 +29,68 @@ namespace FactionColonies
 
 		private bool CanNotReach() => lord.ownedPawns.All(pawn => pawn.carryTracker.CarriedThing == null) && lord.ownedPawns[0].mindState?.duty.def != DutyDefOf.ExitMapBestAndDefendSelf && lord.ownedPawns[0].CanReach(lord.ownedPawns[0].CurJob.targetA, PathEndMode.OnCell, PawnUtility.ResolveMaxDanger(lord.ownedPawns[0], Danger.Some), false, false, TraverseMode.ByPawn);
 
+		private Transition DeliveryToFightTransition(StateGraph stateGraph) => new Transition(stateGraph.StartingToil, stateGraph.lordToils[1])
+		{
+			triggers = new List<Trigger>(2)
+			{
+				new Trigger_PawnHarmed(),
+				new Trigger_Custom((TriggerSignal s) => Map.dangerWatcher.DangerRating == StoryDanger.High)
+			},
+			preActions = new List<TransitionAction>
+			{
+				new TransitionAction_Custom(() => Messages.Message("deliveryPawnsEngageEnemy".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent))
+			}
+		};
+
+		/// <summary>
+		/// This <c>Transition</c> fixes a bug, where sometimes some <c>Pawn</c>s don't get transitioned into this phase even though they should
+		/// </summary>
+		/// <param name="stateGraph"></param>
+		/// <returns></returns>
+		private Transition RefreshFightTransition(StateGraph stateGraph) => new Transition(stateGraph.lordToils[1], stateGraph.lordToils[1], true)
+		{
+			triggers = new List<Trigger>(2)
+			{
+				new Trigger_PawnHarmed(),
+				new Trigger_Custom((TriggerSignal _) => Map.dangerWatcher.DangerRating == StoryDanger.High)
+			}
+		};
+
+		private Transition FightingToLeavingTransition(StateGraph stateGraph) => new Transition(stateGraph.lordToils[1], stateGraph.lordToils[2])
+		{
+			triggers = new List<Trigger>
+			{
+				new Trigger_TicksPassedAndNoRecentHarm(1000)
+			},
+			preActions = new List<TransitionAction>
+			{
+				new TransitionAction_Custom(() => Messages.Message(Map.HasWoundedForFaction(lord.faction) ? "pawnsLeavingMapWithDowned".Translate() : "pawnsLeavingMap".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent))
+			}
+		};
+
+		/// <summary>
+		/// This <c>Transition</c> covers the case in which the delivering pawns can't find a path to the delivery tax spot, for example if the locks mod is used.
+		/// Without this transition, pawns end up dropping their delivery and starving to death
+		/// </summary>
+		/// <param name="stateGraph"></param>
+		/// <returns></returns>
+		private Transition CanNotDeliverToLeavingTransition(StateGraph stateGraph) => new Transition(stateGraph.lordToils[0], stateGraph.lordToils[2])
+		{
+			triggers = new List<Trigger>(1) { new Trigger_Custom((TriggerSignal _) => CanNotReach()) },
+			preActions = new List<TransitionAction>(1) { new TransitionAction_Custom(() => Messages.Message("pawnsLeavingMapNoPath".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent)) }
+		};
+
 		public override StateGraph CreateGraph()
 		{
-			StateGraph stateGraph = new StateGraph
-			{
-				StartingToil = new LordToil_DeliverSupplies()
-			};
+			StateGraph stateGraph = new StateGraph { StartingToil = new LordToil_DeliverSupplies() };
 
 			stateGraph.AddToil(new LordToil_HuntEnemies(fallbackLocation));
 			stateGraph.AddToil(new LordToil_TakeWoundedAndLeave());
 
-			
-			stateGraph.AddTransition(new Transition(stateGraph.StartingToil, stateGraph.lordToils[1])
-			{
-				triggers = new List<Trigger>
-				{
-					new Trigger_PawnHarmed(),
-					new Trigger_Custom((TriggerSignal s) => Map.dangerWatcher.DangerRating == StoryDanger.High)
-
-				}, preActions = new List<TransitionAction>
-				{
-					new TransitionAction_Custom(delegate ()
-					{
-						Messages.Message("deliveryPawnsEngageEnemy".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent);
-					})
-				}
-			});
-
-			stateGraph.AddTransition(new Transition(stateGraph.lordToils[1], stateGraph.lordToils[1], true)
-			{
-				triggers = new List<Trigger>
-				{
-					new Trigger_PawnHarmed(),
-					new Trigger_Custom((TriggerSignal s) => Map.dangerWatcher.DangerRating == StoryDanger.High)
-				}
-			});
-
-			stateGraph.AddTransition(new Transition(stateGraph.lordToils[1], stateGraph.lordToils[2])
-			{
-				triggers = new List<Trigger>
-				{
-					new Trigger_TicksPassedAndNoRecentHarm(1000)
-				},
-				preActions = new List<TransitionAction>
-				{
-					new TransitionAction_Custom(delegate ()
-					{
-						if (Map.HasWoundedForFaction(lord.faction))
-						{
-							Messages.Message("pawnsLeavingMapWithDowned".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent);
-						}
-						else
-						{
-							Messages.Message("pawnsLeavingMap".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent);
-						}
-					})
-				}
-			});
-
-			stateGraph.AddTransition(new Transition(stateGraph.lordToils[0], stateGraph.lordToils[2])
-			{
-				triggers = new List<Trigger>
-				{
-					new Trigger_Custom(delegate(TriggerSignal signal) 
-					{
-						return CanNotReach();
-					})
-				},
-				preActions = new List<TransitionAction>
-				{
-					new TransitionAction_Custom(delegate ()
-					{
-						Messages.Message("pawnsLeavingMapNoPath".Translate(), lord.ownedPawns, MessageTypeDefOf.NeutralEvent);
-					})
-				}
-			});
+			stateGraph.AddTransition(DeliveryToFightTransition(stateGraph));
+			stateGraph.AddTransition(RefreshFightTransition(stateGraph));
+			stateGraph.AddTransition(FightingToLeavingTransition(stateGraph));
+			stateGraph.AddTransition(CanNotDeliverToLeavingTransition(stateGraph));
 
 			return stateGraph;
 		}
