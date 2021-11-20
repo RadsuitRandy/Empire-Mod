b19a36b24d3000b298769deb3a434d6e9442de6f
Daniel Plaster <daniel_plaster@live.de>
2021-11-20 19:50:55 +0100

    moved and organized HarmonyPatches

---
+++ 1.3/Source/FactionColonies/HarmonyPatches/CommsConsolePatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FactionColonies
+{
+    /// <summary>
+ /// This patch disables the military aid a player can receive through the comms console for our faction
+ /// </summary>
+    [HarmonyPatch(typeof(FactionDialogMaker), "RequestMilitaryAidOption")]
+    class DisableMilitaryAid
+    {
+        static void Postfix(Map map, Faction faction, Pawn negotiator, ref DiaOption __result)
+        {
+            if (faction.def.defName != "PColony") return;
+            __result = new DiaOption("RequestMilitaryAid".Translate(25));
+            __result.Disable("Disabled. Use the settlements military tab.");
+        }
+    }
+}
---
+++ 1.3/Source/FactionColonies/HarmonyPatches/PawnPatches.cs
@@ -0,0 +1,82 @@
+﻿using FactionColonies.util;
+using HarmonyLib;
+using Verse;
+
+namespace FactionColonies
+{
+    [HarmonyPatch(typeof(Pawn), "Kill")]
+    class MercenaryDied
+    {
+        static bool Prefix(Pawn __instance)
+        {
+            if (__instance.IsMercenary())
+            {
+                if (__instance.Faction != FactionColonies.getPlayerColonyFaction()) __instance.SetFaction(FactionColonies.getPlayerColonyFaction());
+                MercenarySquadFC squad = Find.World.GetComponent<FactionFC>().militaryCustomizationUtil.returnSquadFromUnit(__instance);
+                if (squad != null)
+                {
+                    Mercenary merc = Find.World.GetComponent<FactionFC>().militaryCustomizationUtil.returnMercenaryFromUnit(__instance, squad);
+                    if (merc != null)
+                    {
+                        if (squad.settlement != null)
+                        {
+                            if (FactionColonies.Settings().deadPawnsIncreaseMilitaryCooldown)
+                            {
+                                squad.dead += 1;
+                            }
+
+                            squad.settlement.happiness -= 1;
+                        }
+
+                        squad.PassPawnToDeadMercenaries(merc);
+                    }
+
+                    squad.removeDroppedEquipment();
+                }
+                else
+                {
+                    Log.Message("Mercenary Errored out. Did not find squad.");
+                }
+
+                __instance.equipment?.DestroyAllEquipment();
+                __instance.apparel?.DestroyAll();
+                //__instance.Destroy();
+                return true;
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(DeathActionWorker_Simple), "PawnDied")]
+    class MercenaryAnimalDied
+    {
+        static bool Prefix(Corpse corpse)
+        {
+            if (Find.World.GetComponent<FactionFC>().militaryCustomizationUtil.AllMercenaryPawns
+                .Contains(corpse.InnerPawn))
+            {
+                //corpse.InnerPawn.SetFaction(FactionColonies.getPlayerColonyFaction());
+                corpse.Destroy();
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    // [HarmonyPatch(typeof(JobGiver_AnimalFlee), "TryGiveJob")]
+    class TryGiveJobFleeAnimal
+    {
+        static bool Prefix(Pawn pawn)
+        {
+            if (Find.World.GetComponent<FactionFC>().militaryCustomizationUtil.AllMercenaryPawns.Contains(pawn))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
